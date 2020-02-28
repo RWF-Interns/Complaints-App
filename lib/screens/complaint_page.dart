31ccf2fa868759a59cc8271d703783b5ef2599a0
JerinFrancisA <jerinfrancis77@gmail.com>
2020-02-28 08:57:03 +0530

    qwe

--- lib/screens/complaint_page.dart
+++ lib/screens/complaint_page.dart
@@ -1,77 +1,252 @@
-import 'package:LoginApp/custom_widgets/my_complaint.dart';
+// import 'package:LoginApp/custom_widgets/my_complaint.dart';
+// import 'package:flutter/material.dart';
+
+// class ComplaintPage extends StatefulWidget {
+//   static const routeName = 'ComplaintApp';
+
+//   @override
+//   _ComplaintPageState createState() => _ComplaintPageState();
+// }
+
+// class _ComplaintPageState extends State<ComplaintPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Color(0xFF655F63),
+//         ),
+//         body: Scrollbar(
+//           child: ListView(
+//             children: <Widget>[
+//               MyComplaint(
+//                 nameAdd: "Subhan",
+//                 comp: "Pipe Leakage",
+//                 dateOfComp: "02/02/2020",
+//                 dept: "CV",
+//                 remark: "Repair",
+//               ),
+//               MyComplaint(
+//                 nameAdd: "Haju",
+//                 comp: "Door ",
+//                 dateOfComp: "01/02/2020",
+//                 dept: "CV",
+//                 remark: "Replace",
+//               ),
+//               MyComplaint(
+//                 nameAdd: "Sara",
+//                 comp: "tubelight not working",
+//                 dateOfComp: "05-02-2020",
+//                 dept: "Elec",
+//                 remark: "Repair",
+//               ),
+//               MyComplaint(
+//                 nameAdd: "Prasad Rao",
+//                 comp: "Window",
+//                 dateOfComp: "21/02/2020",
+//                 dept: "Civil",
+//                 remark: "Replace",
+//               ),
+//               MyComplaint(
+//                 nameAdd: "Prasad Rao",
+//                 comp: "Window",
+//                 dateOfComp: "21/02/2020",
+//                 dept: "Civil",
+//                 remark: "Replace",
+//               ),
+//               MyComplaint(
+//                 nameAdd: "Prasad Rao",
+//                 comp: "Window",
+//                 dateOfComp: "21/02/2020",
+//                 dept: "Civil",
+//                 remark: "Replace",
+//               ),
+//               MyComplaint(
+//                 nameAdd: "Prasad Rao",
+//                 comp: "Window",
+//                 dateOfComp: "21/02/2020",
+//                 dept: "Civil",
+//                 remark: "Replace",
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ComplaintPage extends StatefulWidget {
-  static const routeName = 'ComplaintApp';
+  ComplaintPage({Key key, this.title}) : super(key: key);
+
+  static const routeName = 'ComplaintPage';
+
+  final String title;
 
   @override
   _ComplaintPageState createState() => _ComplaintPageState();
 }
 
+List rows = [];
+List<String> colNames = [];
+int noOfColumns = 0;
+List<DataColumn> colHeaders = [];
+List<DataRow> tuples = [];
+Map tuples2 = {};
+
 class _ComplaintPageState extends State<ComplaintPage> {
+  getData() async {
+    colNames = [];
+    var url = 'http://3ee809db.ngrok.io';
+    var response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    var data = response.body;
+    //debugPrint(data);
+
+    Map valueMap = json.decode(data);
+    rows = valueMap['items'];
+    for (int i = 0; i < rows[0].length; i++) {
+      var temp = rows[0].keys.toList()[i];
+      temp = temp[0].toUpperCase() + temp.substring(1);
+      for (int i = 0; i < temp.length; i++) {
+        if (temp[i] == '_') {
+          temp = temp.substring(0, i) +
+              ' ' +
+              temp[i + 1].toUpperCase() +
+              temp.substring(i + 2);
+        }
+      }
+      // colNames.add(rows[0].keys.toList()[i]);
+      colNames.add(temp);
+    }
+    print(colNames);
+  }
+
+  @override
+  void initState() {
+    getData();
+    colHeaders = getColNames();
+    super.initState();
+  }
+
+  var dummyTable = DataTable(
+    columns: [
+      DataColumn(label: Text('Waiting Data...')),
+      DataColumn(label: Text('Waiting Data...')),
+      DataColumn(label: Text('Waiting Data...')),
+      DataColumn(label: Text('Waiting Data...')),
+    ],
+    rows: [
+      DataRow(cells: [
+        DataCell(Text('...')),
+        DataCell(Text('...')),
+        DataCell(Text('...')),
+        DataCell(Text('...')),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('...')),
+        DataCell(Text('...')),
+        DataCell(Text('...')),
+        DataCell(Text('...')),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('...')),
+        DataCell(Text('...')),
+        DataCell(Text('...')),
+        DataCell(Text('...')),
+      ]),
+    ],
+  );
+
+  var originalTable;
   @override
   Widget build(BuildContext context) {
+    getColNames();
+    originalTable = DataTable(
+      columns: colHeaders.length > 0
+          ? colHeaders
+          : List<DataColumn>.generate(
+              6, (i) => DataColumn(label: Text('Waiting'))),
+      rows: tuples ??
+          [
+            DataRow(cells: [
+              DataCell(Text('...')),
+              DataCell(Text('...')),
+              DataCell(Text('...')),
+              DataCell(Text('...')),
+              DataCell(Text('...')),
+              DataCell(Text('...')),
+            ]),
+          ],
+    );
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Color(0xFF655F63),
+          title: Text("Registered Complaints"),
         ),
-        body: Scrollbar(
-          child: ListView(
-            children: <Widget>[
-              MyComplaint(
-                nameAdd: "Subhan",
-                comp: "Pipe Leakage",
-                dateOfComp: "02/02/2020",
-                dept: "CV",
-                remark: "Repair",
-              ),
-              MyComplaint(
-                nameAdd: "Haju",
-                comp: "Door ",
-                dateOfComp: "01/02/2020",
-                dept: "CV",
-                remark: "Replace",
-              ),
-              MyComplaint(
-                nameAdd: "Sara",
-                comp: "tubelight not working",
-                dateOfComp: "05-02-2020",
-                dept: "Elec",
-                remark: "Repair",
-              ),
-              MyComplaint(
-                nameAdd: "Prasad Rao",
-                comp: "Window",
-                dateOfComp: "21/02/2020",
-                dept: "Civil",
-                remark: "Replace",
-              ),
-              MyComplaint(
-                nameAdd: "Prasad Rao",
-                comp: "Window",
-                dateOfComp: "21/02/2020",
-                dept: "Civil",
-                remark: "Replace",
-              ),
-              MyComplaint(
-                nameAdd: "Prasad Rao",
-                comp: "Window",
-                dateOfComp: "21/02/2020",
-                dept: "Civil",
-                remark: "Replace",
-              ),
-              MyComplaint(
-                nameAdd: "Prasad Rao",
-                comp: "Window",
-                dateOfComp: "21/02/2020",
-                dept: "Civil",
-                remark: "Replace",
-              ),
-            ],
-          ),
+        body: ListView(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.all(8.0),
+              child: noOfColumns > 0 ? originalTable : dummyTable,
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            setState(() {});
+          },
+          tooltip: 'Refresh',
+          child: Icon(Icons.refresh),
         ),
       ),
     );
+  }
+
+  static List<DataColumn> getColNames() {
+    List<DataColumn> colChildren = [];
+    for (int i = 0; i < colNames.length; i++) {
+      colChildren.add(DataColumn(label: Text(colNames[i])));
+    }
+    print(colChildren.length);
+    noOfColumns = colChildren.length;
+    print(colChildren);
+    colHeaders = colChildren;
+    getTuples();
+    return colChildren;
+  }
+
+  static getTuples() {
+    List<DataRow> temp = [];
+    for (int i = 0; i < rows.length; i++) {
+      temp.add(
+        DataRow(
+          cells: List<DataCell>.generate(
+            rows[i].length,
+            (j) => DataCell(
+              Text(rows[i].values.toList()[j] ?? 'NULL'),
+            ),
+          ),
+        ),
+      );
+    }
+    tuples = temp;
+  }
+
+  static getTuples2() {
+    Map temp = {};
+    for (int i = 0; i < rows.length; i++) {
+      temp[i] = List<String>.generate(
+        rows[i].length,
+        (j) => rows[i].values.toList()[j] ?? 'NULL',
+      );
+    }
+    tuples2 = temp;
   }
 }
